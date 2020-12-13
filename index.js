aeb225abf60abe51d43ff84130cd1bb9b3499709
Angel Rodriguez <angel41023@hotmail.com>
2020-12-13 14:48:01 -0600

    Initial configuration

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const productsRouter = require('./routes/products')
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
+app.use('/products', productsRouter)
+
+
+const server = app.listen(3000, () => {
+  console.log(`Listening http://localhost:${server.address().port}`)
+})
