643c69218c884d5d574780d2e4e3e1bdee5886d2
Angel Rodriguez <angel41023@hotmail.com>
2020-12-13 15:21:03 -0600

    bulma added and initial layout from /products

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const products = [
   {
     name: "Red shoes",
-    price: 75
+    price: 75,
+    image: "https://www.thegoodwillout.com/media/catalog/product/cache/bf41ca181e831641a2b4a7a9a89e5c7f/n/i/nike-air-yeezy-2-sp-red-october-508214-660-2.jpg"
   },
   {
     name: "Black shoes",
-    price: 125
+    price: 125,
+    image: "https://i1.adis.ws/i/office/2003131840_ld1.jpg?$highres$?fmt.jpeg.interlaced=true"
   }
 ]
 
