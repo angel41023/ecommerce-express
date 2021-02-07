8fdb282556bd54a233d33ee4c8dd9a11f9f5159b
Angel Rodriguez <angel41023@hotmail.com>
2021-02-06 22:21:59 -0600

    [ADD]:heavy_plus_sign: seed user admin

--- config/index.js
+++ config/index.js
@@ -6,7 +6,11 @@ const config = {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
+  dbName: process.env.DB_NAME,
+  authAdminUsename: process.env.AUTH_ADMIN_USERNAME,
+  authAdminPassword: process.env.AUTH_ADMIN_PASSWORD,
+  authAdminEmail: process.env.AUTH_ADMIN_EMAIL,
+  authAdminSecret: process.env.AUTH_ADMIN_SECRET
 }
 
 module.exports = { config }
